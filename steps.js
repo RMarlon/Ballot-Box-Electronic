4fbd2e93bdc8d41920b1b4c1a4e119693014cf56
Marlon Santos <xsantos.marlonx@gmail.com>
2024-07-14 11:24:09 -0300

    Creating full structure of votation

---
+++ steps.js
@@ -0,0 +1,51 @@
+let steps = [
+    {
+        title: 'Councilman',
+        number: 5,
+        candidates: [
+            {
+                number: 38111,
+                name: 'Hilda de Polares',
+                party: 'ODN',
+                picture: [
+                    { url: 'Hilda.png', legend: 'Councilman' }
+                ]
+            },
+            {
+                number: 77222,
+                name: 'Siegfried de Doube',
+                party: 'ODN',
+                picture: [
+                    { url: 'Siegfried.png', legend: 'councilman' }
+                ]
+            },
+        ]
+    },
+    {
+        title: 'Prefect',
+        number: 2,
+        candidates: [
+            {
+                number: 99,
+                name: 'Saori',
+                party: 'ATN',
+                vice: 'Shion',
+                picture: [
+                    { url: 'Saori.png', legend: 'Prefect' },
+                    { url: 'Shion.png', legend: 'Vice-Mayor', small: false }
+                ]
+            },
+            {
+                number: 88,
+                name: 'Hades',
+                party: 'IPI',
+                vice: 'Radamanthys',
+                picture: [
+                    { url: 'Hades.png', legend: 'Prefect' },
+                    { url: 'Hadamanthys.png', legend: 'Vice-Mayor', small: false }
+                ]
+            },
+
+        ]
+    }
+]
