5709c0a6eec5fd1791321682db08ff689bfabd05
Marlon Santos <xsantos.marlonx@gmail.com>
2024-07-17 14:44:27 -0300

    Done the functionalities of the buttons, white, correct, and confirm

--- scrript.js
+++ scrript.js
@@ -7,16 +7,19 @@ let numbers = document.querySelector('.d-1-3');
 
 let actualSteps = 0;
 let num = '';
+let white = false;
 
 function startSteps() {
     let step = steps[actualSteps];
     let numberHtml = '';
+    num = '';
+    white = false;
 
     for (let i = 0; i < step.number; i++) {
-        if(i === 0){
-            numberHtml += '<div class="number flashes"></div>';    
+        if (i === 0) {
+            numberHtml += '<div class="number flashes"></div>';
         }
-        else{
+        else {
             numberHtml += '<div class="number"></div>';
         }
     }
@@ -31,15 +34,15 @@ function startSteps() {
 
 function actualizeInterface() {
     let step = steps[actualSteps];
-    let candidate = step.candidates.filter((item)=>{
-        if(item.number === num){
+    let candidate = step.candidates.filter((item) => {
+        if (item.number === num) {
             return true;
         }
-        else{
+        else {
             return false;
         }
     });
-    if(candidate.length > 0){
+    if (candidate.length > 0) {
         candidate = candidate[0];
 
         yourVoteFor.style.display = 'block';
@@ -47,45 +50,74 @@ function actualizeInterface() {
         description.innerHTML = `Name: ${candidate.name}<br/> Party: ${candidate.party}`;
 
         let pictureHtml = '';
-        for(let i in candidate.picture){
+        for (let i in candidate.picture) {
+            if(candidate.picture[i].small){
+                pictureHtml += `<div class="d-1-image small"> <img src="/images/${candidate.picture[i].url}" alt="">${candidate.picture[i].legend}</div>`;
+            }
             pictureHtml += `<div class="d-1-image"> <img src="/images/${candidate.picture[i].url}" alt="">${candidate.picture[i].legend}</div>`;
 
         }
         side.innerHTML = pictureHtml;
     }
-    else{
+    else {
         yourVoteFor.style.display = 'block';
         notice.style.display = 'block';
-        description.innerHTML ='<div class="big--side flashes">Vote Null</div>';
+        description.innerHTML = '<div class="big--side flashes">Vote Null</div>';
     }
 }
 
 function cliked(n) {
     let elNumber = document.querySelector('.number.flashes');
-    if(elNumber !== null){
+    if (elNumber !== null) {
         elNumber.innerHTML = n;
         num = `${num}${n}`;
 
         elNumber.classList.remove('flashes');
-        if(elNumber.nextElementSibling !== null){
+        if (elNumber.nextElementSibling !== null) {
             elNumber.nextElementSibling.classList.add('flashes');
         }
-        else{
+        else {
             actualizeInterface();
         }
     }
 }
 
-function white() {
-    console.log('cliked in white');
+function blankVote() {
+    num = '';
+    white = true;
+    yourVoteFor.style.display = 'block';
+    notice.style.display = 'block';
+    numbers.innerHTML = '';
+    description.innerHTML = '<div class="big--side flashes">Blank Vote</div>';
+    side.innerHTML = '';
 }
 
 function correct() {
-    console.log('clicked in correct');
+    startSteps();
 }
 
 function confirmOK() {
-    console.log('cliked in confirm');
+    let step =steps[actualSteps];
+    let confirmationVote = false;
+
+    if(white === true){
+        confirmationVote = true;
+        console.log('Confirming with white...');
+    }
+    else if(num.length === step.number){
+        confirmationVote = true;
+        console.log('Confirming with ' + num)
+    }
+    
+    if(confirmationVote){
+        actualSteps++;
+        if(steps[actualSteps] !== undefined){
+            startSteps();
+        }
+        else{
+            console.log('FIM');
+        }
+    }
 }
 
 startSteps();
